aa920b04cbf117a463a9f1320909d575a6ab4596
inkdrive <fractal.ink.drive@gmail.com>
2025-01-03 12:33:57 +0530

    Create env.js

---
+++ env.js
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = 'https://evaflxplucbfidlxyoaq.supabase.co';
+export const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV2YWZseHBsdWNiZmlkbHh5b2FxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzU4ODc2OTcsImV4cCI6MjA1MTQ2MzY5N30.Y7FtDEl5a1iiKfCRXeE7vp1pBo_ZXod8wBEID-1H_FI';
+
